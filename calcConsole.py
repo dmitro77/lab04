827315b05535a9f4e89f8a40d7086142c074a7bf
dmitro77 <dimaermakov151@gmail.com>
2024-09-24 13:58:35 +0300

    Done

---
+++ calcConsole.py
@@ -0,0 +1,5 @@
+import sys
+
+args = sys.argv
+result = str(float(args[1]) * float(args[2]) / 2 * float(args[3]))
+print("Обєм призми є: " + result)
---
+++ calcInput.py
@@ -0,0 +1,7 @@
+inputText = input("Уведіть чотиризначне число:")
+
+sumResult = str(float(inputText[1]) + float(inputText[2]))
+multResult = str(float(inputText[1]) * float(inputText[2]))
+
+print("Сума: " + sumResult)
+print("Добуток: " + sumResult)
